f4eef315f1bcb5b8fac27f9dcf5eabd1cb436447
Skripchenko-Nikolai <81862624+Skripchenko-Nikolai@users.noreply.github.com>
2022-04-23 21:19:20 +0600

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/city")]
+    public class CityController : ControllerBase
+    {
+
+       [HttpGet]
+       public List<City> getCity()
+       {
+            return City.GetAllCity();
+       }
+
+        [HttpGet("{id}")]
+        public City getCityById(int id)
+        {
+            return City.GetCityById(id);
+        }
+    }
+}
---
+++ WebApplication1/Coords.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Coords
+
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("lat")]
+        public string Lat { get; set; }
+
+        [JsonPropertyName("lon")]
+        public string Lon { get; set; }
+    }
+}
